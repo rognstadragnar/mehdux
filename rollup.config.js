b28e241a50fd52e4643b01aad87794b72746b5e0
Ragnar Rognstad <Ragnar.Rognstad@nrk.no>
2018-01-28 10:14:51 +0100

    Remove uneeded things from rollup-starter-lib

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,15 @@
 import resolve from 'rollup-plugin-node-resolve'
-// import commonjs from 'rollup-plugin-commonjs'
 import babel from 'rollup-plugin-babel'
 import pkg from './package.json'
 
 export default [
-  // browser-friendly UMD build
   {
     entry: 'src/main.js',
     dest: pkg.browser,
     format: 'umd',
-    moduleName: 'howLongUntilLunch',
+    moduleName: 'aoudad',
     plugins: [
-      resolve(), // so Rollup can find `ms`
+      resolve(),
       babel({
         exclude: ['node_modules/**']
       })
